0e9880bcf2bbd6c71b88a6b84df90fc643ab3853
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-09-11 11:07:18 +0100

    add methods to query presence of arguments on a CLI::Command class

--- test/command_test.rb
+++ test/command_test.rb
@@ -187,5 +187,47 @@ EOF
         end
       end
     end
+    describe "testing that an argument with certain properties is expected" do
+      before(:all) do
+        @example = Class.new(Climate::Command) do
+          name 'test'
+          arg :required_argument, "A required argument"
+          arg :optional_argument, "An optional argument", :required => false
+          arg :multiple_argument, "An argument with multiple values", :multi => true, :required => false
+        end
+      end
+      describe "has_argument?" do
+        it 'returns true when the named argument is defined' do
+          assert @example.has_argument?(:optional_argument)
+        end
+        it 'returns false when the named arguement is not defined' do
+          refute @example.has_argument?(:nonexistent_argument)
+        end
+      end
+      describe "has_required_argument?" do
+        it 'returns true when the named argument is defined and required' do
+          assert @example.has_required_argument?(:required_argument)
+        end
+        it 'returns false when the named argument is defined and not required' do
+          refute @example.has_required_argument?(:optional_argument)
+        end
+
+        it 'returns false when the named argument is not defined' do
+          refute @example.has_required_argument?(:nonexistent_argument)
+        end
+      end
+      describe "has_multi_argument?" do
+        it 'returns true when the named argument is defined and takes multiple values' do
+          assert @example.has_multi_argument?(:multiple_argument)
+        end
+        it 'returns false when the named argument is defined and not multiple' do
+          refute @example.has_multi_argument?(:optional_argument)
+        end
+
+        it 'returns false when the named argument is not defined' do
+          refute @example.has_multi_argument?(:nonexistent_argument)
+        end
+      end
+    end
   end
 end

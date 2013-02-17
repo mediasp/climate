8e03806bc0e7e508ca4ad25d9f8fd782ede45eea
Nick Griffiths <nicobrevin@gmail.com>
2013-02-17 15:04:34 +0000

    extract error handling for testability

--- lib/climate.rb
+++ lib/climate.rb
@@ -2,31 +2,40 @@ require 'trollop'
 
 module Climate
 
-  def self.with_standard_exception_handling(options={}, &block)
+  def self.error_messages
     error_messages = {
       UnexpectedArgumentError => 'Unknown argument',
       UnknownCommandError => 'Unknown command',
       MissingArgumentError => 'Missing argument',
       ConflictingOptionError => 'Conflicting options given'
     }
+  end
 
+  def self.with_standard_exception_handling(options={}, &block)
     begin
       yield
-    rescue ExitException => e
+    rescue => e
+      exit handle_error(e, options)
+    end
+  end
+
+  def self.handle_error(e, options)
+    case e
+    when ExitException
       # exit silently if there is no error message to print out
       $stderr.puts(e.message) if e.has_message?
-      exit(e.exit_code)
-    rescue HelpNeeded => e
+      e.exit_code
+    when HelpNeeded
       print_usage(e.command_class, options)
-      exit(0)
-    rescue ParsingError => e
+      0
+    when ParsingError
       $stderr.puts(error_messages[e.class] + ": #{e.message}")
       print_usage(e.command_class, options)
-      exit(1)
-    rescue => e
+      1
+    else
       $stderr.puts("Unexpected error: #{e.class.name} - #{e.message}")
       $stderr.puts(e.backtrace)
-      exit(2)
+      2
     end
   end
 
